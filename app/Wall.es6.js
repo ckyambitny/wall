751bfc8a64ed04f30e45a507ff0ffb1c7bcb408e
Bartłomiej Sobczuk <bartlomiej.sobczuk@gmail.com>
2015-06-01 17:10:29 +0200

    ll

--- app/Wall.es6.js
+++ app/Wall.es6.js
@@ -1,13 +1,21 @@
 'use strict';
+
 class Wall {
     constructor() {
         this.height = window.innerHeight;
-        this.imageNumber = Math.floor(this.height / 200) +1; 
+        this.imagesNumber = Math.floor(this.height / 200) +1; 
         this.wraper = document.querySelector('.wraper');
         this.wraper.style.width = '960px';
-        this.startHeight = this.imageNumber * 210 + 'px';
+        this.startHeight = this.imagesNumber * 210 + 'px';
         this.wraper.style.height = this.startHeight;
-        console.log(this.imageNumber);
+        console.log(this.imagesNumber);
+        this.appendImages(this.imagesNumber);
+    }    
+    appendImages(number) {
+        for(var i = 0; i < number; i++) {
+            let d = document.createElement('DIV');
+            this.wraper.appendChild(d);
+        } 
     }
 }
 
