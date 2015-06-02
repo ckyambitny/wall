475f59e351cafbb34a9e4a097ea96a31bd3868fa
Piotr Kowalski <piecioshka@gmail.com>
2015-06-02 15:30:25 +0200

    Support adding comments.

--- app/scripts/Wall.es6.js
+++ app/scripts/Wall.es6.js
@@ -8,14 +8,14 @@ class Wall {
         let windowHeight = window.innerHeight;
         let imagesNumber = Math.floor(windowHeight / 210) + 1;
         this.wrapper = document.querySelector('.wrapper');
-        this.posts =[];
+        this.posts = [];
         // Add images to page.
         this.appendPosts(imagesNumber);
 
         // Listen for user scrolling down.
         window.addEventListener('scroll', (e) => {
             // Update window height (could be change).
-            windowHeight = window.innerHeight; 
+            windowHeight = window.innerHeight;
 
             let documentHeight = document.documentElement.offsetHeight;
             let sY = window.scrollY;
@@ -34,42 +34,58 @@ class Wall {
     appendPosts(number) {
         for (let i = 0; i < number; i++) {
             this.appendPost();
-        } 
+        }
     }
-    
-    // TODO: try to implement for of
-    addListener(el) {
-        
-        for (let i = 0; i < el.length ; i++ ) {
-            let input = el[i].lastElementChild.lastElementChild;
-            //console.log(input);
-            let comments = [];
-            input.addEventListener('keypress', function (e) {
-                if ( e.keyCode == '13') {
-                    if ( input.value.trim().length > 0 ) {
-                        //comments.push(JSON.stringify(el[i].value.trim()));
-                        //document.querySelector()     
-                        //console.log(JSON.stringify(input.value));                   
-                        //console.log(el[i]);
-                        comments.push(input.value);
-                        input.value ='';
-                        console.log(comments);
-                        
-                    }   
-                }                    
-            });
-        }      
+
+    addListener($element) {
+        if (!$element) {
+            throw new Error('$element is not exists');
+        }
+
+        let input = $element.lastElementChild.lastElementChild;
+        let comments = [];
+
+        input.addEventListener('keypress', function (e) {
+            if (e.keyCode == '13') {
+                let comment = input.value.trim();
+
+                if (comment.length > 0) {
+                    // Add to list of comments.
+                    comments.push(comment);
+
+                    // Clear input.
+                    input.value = '';
+
+                    let $div = document.createElement('div');
+                    $div.classList.add('list-group-item');
+                    $div.innerText = comment;
+                    $element.querySelector('.comments').appendChild($div);
+                }
+            }
+        });
     }
-        
+
     appendPost() {
         console.log('appendPost');
+
         let u = 'http://placeskull.com/950/200';
-        let src = u + '?' + Math.random();
-        let context = {url: src};
-        let adding = template(context) + '';
-        this.wrapper.innerHTML +=  adding;
-        let inputBox = document.querySelectorAll('.post');
-        this.addListener(inputBox);
+        let url = u + '?' + Math.random();
+        let id = 'id-' + this._uniqueId;
+        let context = { id, url };
+        this.wrapper.innerHTML += String(template(context));
+
+        requestAnimationFrame(() => {
+            let $post = document.querySelector('#' + id);
+            this.addListener($post);
+        });
+    }
+
+    get _uniqueId() {
+        if (!this._uid) {
+            this._uid = 0;
+        }
+
+        return ++this._uid;
     }
 }
 
