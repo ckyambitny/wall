177b8525b1a94e2529097ab5432a32b9339ce953
Bartłomiej Sobczuk <bartlomiej.sobczuk@gmail.com>
2015-06-03 02:49:05 +0200

    added saving to localStorage, many debug comments

--- app/scripts/Wall.es6.js
+++ app/scripts/Wall.es6.js
@@ -5,7 +5,7 @@ import template from '../templates/post.handlebars';
 class Wall {
     constructor() {
         // Object with key (as post id) and value (as post object).
-        this.posts = {};
+        this.posts = {}; 
 
         let windowHeight = window.innerHeight;
         let imagesNumber = Math.floor(windowHeight / 210) + 1;
@@ -63,20 +63,29 @@ class Wall {
         $post.querySelector('.comments').appendChild($div);
 
         // TODO: push new comment to comments list in post object.
-        // ...
+        // get id of $post were adding comment
+        let $id = $post.id;
+        //debug CL
+        //console.log($id);
+        //console.log(this.posts[$id]);//Great return :D!!!!!
+         
+        //change value of given id in this,posts by adding new comment to comment list
+        this.posts[$id].commentList.push(comment);
 
+        // just to be sure were saving ok console log all commentList for given post when comment added    
+        console.log(this.posts[$id].commentList);
         // Save data to storage.
         this.save();
     }
 
     addPost() {
         let u = 'http://placeskull.com/950/200';
-        var random = String(Math.random()).slice(2);
+        var random = String(Math.random()).slice(2);//why not let?
         let url = u + '?' + random;
-        let id = 'id-' + random;
+        let id = 'id-' + random;//this probably should be $id, but handlebars etc.
         let context = { id, url };
-
-        console.log('addPost id=%s', id);
+        //ASK %s C type console log
+        //console.log('addPost id=%s', id);
 
         // Render
         let $fragment = document.createElement('div');
@@ -88,7 +97,16 @@ class Wall {
         this.addListener($post);
 
         // TODO: save post object to `this.posts`.
-        // ...
+        let commentList = [];
+        //debug addings
+        //commentList.push('LOL');
+        //commentList.push('Ajwaj');
+        //console.log(id);
+        //adding debug LOL, Ajwaj comments to list to console log it out
+        this.posts[id] = { commentList, url };// not this.posts.id - easy to mix up with let id 8X line
+        //debug log
+        //console.log(this.posts.id);
+        //console.log(this.posts.id.commentList[1]);//returning great:D
 
         // Save data to storage.
         this.save();
@@ -96,6 +114,7 @@ class Wall {
 
     save() {
         // TODO: Save `this.posts` to localStorage.
+        localStorage.setItem('posts', JSON.stringify(this.posts));
     }
 
     static isEnter(e) {
